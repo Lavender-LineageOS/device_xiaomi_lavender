27add9a2afbdbe555d4503ac14e17efd5ea97e96
pix106 <sbordenave@gmail.com>
2024-05-14 21:28:06 -0300

    lavender: Update DRM blobs from  Zebra Helios * From Zebra/helios/helios:13/13-22-18.00-TG-U02-STD-HEL-04/84:user/release-keys

--- device.mk
+++ device.mk
@@ -133,10 +133,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
-
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true

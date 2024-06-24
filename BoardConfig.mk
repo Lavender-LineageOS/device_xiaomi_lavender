dbf5780bf966f887138fc138bea9a7e2e3060a78
mikeNG <>
2024-06-24 14:00:43 +0000

    lavender: Include lineage FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,9 +77,9 @@ LOC_HIDL_VERSION := 3.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
-
+   hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+   hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+   vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml

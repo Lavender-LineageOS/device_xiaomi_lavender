212e26f910249f884d711599c65f9cf16f283a3b
SebaUbuntu <seba@sebaubuntu.dev>
2024-06-24 14:28:49 +0000

    lavender: Move to common Xiaomi light AIDL

--- device.mk
+++ device.mk
@@ -258,7 +258,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
+    android.hardware.light-service.xiaomi
 
  # Low power Whitelist
 PRODUCT_COPY_FILES += \

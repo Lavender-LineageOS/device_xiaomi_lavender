1839af743563a2411c948c61b6b55f02b6e7c6ad
bgcngm <>
2024-06-24 13:55:10 +0000

    lavender: Inherit from QTI and common Xiaomi FCMs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,10 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
